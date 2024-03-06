a6d2fa3e750836219b125fb6f24b5b3861d237dc
Ant Mar <antonio.maringonzalez@telefonica.com>
2024-03-06 22:19:01 +0100

    subo sudoku2

---
+++ Sudoku2/src/pack/Dificultad.java
@@ -0,0 +1,19 @@
+package pack;
+
+public enum Dificultad {
+
+	FACIL, MEDIO, DIFICIL;
+	
+//	public static Dificultad getDificultad(String instruction) {
+//        switch (instruction.toLowerCase()) {
+//            case "facil":
+//                return FACIL;
+//            case "medio":
+//                return MEDIO;
+//            case "dificil":
+//                return DIFICIL;
+//            default:
+//                throw new IllegalArgumentException("Invalid instruction: " + instruction);
+//        }
+//    }
+}
